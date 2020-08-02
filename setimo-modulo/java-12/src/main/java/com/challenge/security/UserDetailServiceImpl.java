79c3e7e323aa94a400b18ad415b456009af8675c
Alexandre Dias <alexbpdias@gmail.com>
2020-08-01 23:23:32 -0300

    Finalizado desafio da setima semana

---
+++ setimo-modulo/java-12/src/main/java/com/challenge/security/UserDetailServiceImpl.java
@@ -0,0 +1,20 @@
+package com.challenge.security;
+
+import com.challenge.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username).get();
+    }
+}
---
+++ setimo-modulo/java-12/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- delete from users where 1=1;
+-- insert into users (id, full_name, email, nickname, password, created_at)
+-- values (1, 'Alexandre', 'alexandre@dias.com', 'alexandredias', '123', now());
+select * from users;
